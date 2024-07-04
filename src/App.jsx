3b3390087d13b6e4e4e89a03040f6998e1be4ab5
Mostafa-m-gomaa <aagomaaa2@gmail.com>
2024-07-04 08:26:17 +0300

    mostafa

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,16 @@ function App() {
   const [login, setLogin] = useState(false);
   const [token, setToken] = useState("");
   const [loader, setLoader] = useState(false);
-  const [route, setRoute] = useState("https://api.theoriehaast.nl/api/v1");
-  const [filesRoute ]=useState("https://api.theoriehaast.nl/questionsImages")
+  // const [route, setRoute] = useState("https://api.theoriehaast.nl/api/v1");
+  // const [route, setRoute] = useState("https://api.theoriehaast.nl/api/v1");
+  const route = import.meta.env.VITE_API_URL;;
+  const filesRoute =import.meta.env.VITE_API_URLFILE;
+  // const [filesRoute ]=useState("https://api.theoriehaast.nl/questionsImages")
+  // const [filesRoute ]=useState("https://api.theoriehaast.nl/questionsImages")
   const [lang, setLang] = useState("en");
   const [answers,setAnswers]=useState([])
   const [exams,setExams]=useState({})
+
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
@@ -42,6 +47,8 @@ function App() {
       changeLanguage(sessionStorage.getItem("lang"))
       setLang(sessionStorage.getItem("lang"))
     }
+    console.log(route)
+    
 
   },[lang])
   useEffect(() => {
